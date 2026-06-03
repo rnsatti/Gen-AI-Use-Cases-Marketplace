10a5f622b343a6c4114756c8c2f4538f497c96e5
rnsatti <56648754+rnsatti@users.noreply.github.com>
2026-06-03 19:16:53 +0400

    Update config.js

--- config.js
+++ config.js
@@ -4,4 +4,4 @@
 // ============================================================
 
 const SUPABASE_URL  = 'https://drverzdjbquldwqpcxdp.supabase.co';
-const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRydmVyemRqYnF1bGR3cXBjeGRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0OTI2NjAsImV4cCI6MjA5NjA2ODY2MH0.4xWdnwlAhcnEbniMjtWIIqYwksyDJKPfByi1bKT_omA';
+const SUPABASE_ANON = 'sb_publishable_PLAfW-nIIhwBBeBizjutYg_spiWmZap';
